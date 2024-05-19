ef7dcf2de96599d39bfa8079ddb3e185f2bad26d
gleeeeison <gvieira-durta@myseneca.ca>
2024-05-19 14:28:16 -0400

    DbSet set up

--- DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
+++ DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
@@ -1,38 +1,23 @@
 ﻿using DevFreela.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace DevFreela.Infrastructure.Persistence
 {
-    public class DevFreelaDbContext
+    public class DevFreelaDbContext : DbContext
     {
-        public DevFreelaDbContext()
+        public DevFreelaDbContext(DbContextOptions<DevFreelaDbContext> options) : base(options)
         {
-            Projects = new List<Project>
-            {
-                new Project("First Project", "my first project", 1, 1, 1000),
-                new Project("Second Project", "my second project", 2, 2, 2000),
-                new Project("Third Project", "my third project", 3, 3, 3000),
-                new Project("Fourth Project", "my fourth project", 4, 4, 4000)
-            };
 
-            Users = new List<User>
-            { 
-                new User("Gleison Dutra", "g@email.com", new DateTime(1990, 1, 31)),
-                new User("Adrian Smith", "a@email.com", new DateTime(1991, 1, 30)),
-                new User("John Doe", "j@email.com", new DateTime(1992, 1, 29)),
-                new User("Mary", "m@email.com", new DateTime(1993, 1, 28))
-            };
-
-            Skills = new List<Skill>
-            { 
-                new Skill(".NET Core"),
-                new Skill("C#"),
-                new Skill("SQL")
-            };
         }
 
-        public List<Project> Projects { get; set; }
-        public List<User> Users { get; set; }
-        public List<Skill> Skills { get; set; }
-        public List<ProjectComment> ProjectComments { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<ProjectComment> ProjectComments { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<User> Users { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
