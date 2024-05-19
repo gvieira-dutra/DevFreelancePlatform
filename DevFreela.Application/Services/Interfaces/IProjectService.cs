c30f198c4d7efbe2afac439f61707633feae6d5b
gleeeeison <gvieira-durta@myseneca.ca>
2024-05-19 14:31:00 -0400

    remove unused directives, replaced input models

--- DevFreela.Application/Services/Interfaces/IProjectService.cs
+++ DevFreela.Application/Services/Interfaces/IProjectService.cs
@@ -1,21 +1,24 @@
-﻿using DevFreela.Application.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DevFreela.Application.InputModels;
+using DevFreela.Application.ViewModels;
 
 namespace DevFreela.Application.Services.Interfaces
 {
     public interface IProjectService
     {
-        List<ProjectViewModel> GetAll();
+        List<ProjectViewModel> GetAll(string query);
+
         ProjectDetailViewModel GetById(int id);
-        int Create(NewProjectInputModel inputModel);
-        int Update(UpdateProjectModel updateModel);
-        void Delete(int id);
+
         void CreateComment(CreateCommentInputModel inputModel);
+
+        int Create(NewProjectInputModel inputModel);
+
+        int Update(UpdateProjectInputModel updateModel);
+
         void Start(int id);
+
         void Finish(int id);
+
+        void Delete(int id);
     }
 }
