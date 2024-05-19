1240712491a183183874f896130f60b3475db990
gleeeeison <gvieira-durta@myseneca.ca>
2024-05-19 14:26:22 -0400

    connection with Services

--- DevFreela.API/Controllers/UsersController.cs
+++ DevFreela.API/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
-﻿using DevFreela.Models;
+﻿using DevFreela.Application.InputModels;
+using DevFreela.Application.Services.Interfaces;
+using DevFreela.Models;
 
-//using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DevFreela.Controllers
@@ -8,25 +9,36 @@ namespace DevFreela.Controllers
     [Route("api/users")]
     public class UsersController : ControllerBase
     {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
         // api/users/1
         [HttpGet("{id}")]
         public IActionResult GetById(int id)
         {
-            return Ok();
+            var fetchedUser = _userService.GetById(id);
+
+            return Ok(fetchedUser);
         }
 
         // api/users
         [HttpPost]
-        public IActionResult Post([FromBody] CreateUserModel createUserModel)
+        public IActionResult Post([FromBody] CreateUserInputModel inputModel)
         {
-            return CreatedAtAction(nameof(GetById), new { id = 1 }, createUserModel);
+            var id = _userService.Create(inputModel);
+
+            return CreatedAtAction(nameof(GetById), new { id = id }, inputModel);
         }
 
         // api/1/login
         [HttpPut("{id}/login")]
         public IActionResult Login(int id, [FromBody] LoginModel loginModel)
         {
-            return NoContent();
+            return NoContent(); 
         }
     }
 }
