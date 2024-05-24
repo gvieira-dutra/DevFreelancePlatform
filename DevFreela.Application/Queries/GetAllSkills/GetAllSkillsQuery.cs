6a35492fe3d960011db598741b41bd1ef455d7f9
gleeeeison <gvieira-durta@myseneca.ca>
2024-05-24 06:51:05 -0400

    CQRS pattern - get all skills

---
+++ DevFreela.Application/Queries/GetAllSkills/GetAllSkillsQuery.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace DevFreela.Application.Queries.GetAllSkills
+{
+    public class GetAllSkillsQuery : IRequest<List<SkillViewModel>>
+    {
+        public GetAllSkillsQuery()
+        {
+        }
+
+    }
+}
---
+++ DevFreela.Application/Queries/GetAllSkills/GetAllSkillsQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Data.SqlClient;
+
+namespace DevFreela.Application.Queries.GetAllSkills
+{
+    internal class GetAllSkillsQueryHandler : IRequestHandler<GetAllSkillsQuery, List<SkillViewModel>>
+    {
+        private readonly string _connectionString;
+
+        public GetAllSkillsQueryHandler(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DevFreelaCs");
+        }
+
+        public async Task<List<SkillViewModel>> Handle(GetAllSkillsQuery request, CancellationToken cancellationToken)
+        {
+            using (var sqlConnection = new SqlConnection(_connectionString))
+            {
+                sqlConnection.Open();
+
+                var script = "SELECT Id, Description " +
+                             "FROM Skills";
+
+                var skills = await sqlConnection.QueryAsync<SkillViewModel>(script);
+
+                return skills.ToList();
+            }
+        }
+    }
+}
