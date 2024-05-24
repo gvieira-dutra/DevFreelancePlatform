2c3ed484410ebc19a772f4973f09c82a88cab2b7
gleeeeison <gvieira-durta@myseneca.ca>
2024-05-24 06:51:41 -0400

    CQRS pattern - get user

---
+++ DevFreela.Application/Queries/GetUser/GetUserQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace DevFreela.Application.Queries.GetUser
+{
+    public class GetUserQuery : IRequest<UserViewModel>
+    {
+        public GetUserQuery(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; private set; }
+    }
+}
