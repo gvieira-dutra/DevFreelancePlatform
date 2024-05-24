35d85fec61893d93eb2527a9f56b689d73ed2027
gleeeeison <gvieira-durta@myseneca.ca>
2024-05-24 06:50:53 -0400

    CQRS pattern - get all projects

---
+++ DevFreela.Application/Queries/GetAllProjects/ProjectViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace DevFreela.Application.Queries.GetAllProjects
+{
+    public class ProjectViewModel
+    {
+        public ProjectViewModel(int id, string title, DateTime createdAt)
+        {
+            Id = id;
+            Title = title;
+            CreatedAt = createdAt;
+        }
+
+        public int Id { get; private set; }
+        public string Title { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+    }
+}
