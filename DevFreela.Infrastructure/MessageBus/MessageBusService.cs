9cd6a89ff3c64d0bdf71f534b5eb20092eb9bc3d
Gleison <126490844+gvieira-dutra@users.noreply.github.com>
2024-09-10 13:27:51 -0400

    removing rabbit MQ so pipeline can work

--- DevFreela.Infrastructure/MessageBus/MessageBusService.cs
+++ DevFreela.Infrastructure/MessageBus/MessageBusService.cs
@@ -5,41 +5,41 @@ namespace DevFreela.Infrastructure.MessageBus
 {
     public class MessageBusService : IMessageBusService
     {
-        private readonly ConnectionFactory _factory;
-        public MessageBusService()
-        {
-            _factory = new ConnectionFactory
-            {
-                HostName = "localhost"
-            };
-        }
+        // private readonly ConnectionFactory _factory;
+        //public MessageBusService()
+        //{
+        //    _factory = new ConnectionFactory
+        //    {
+        //        HostName = "localhost"
+        //    };
+        //}
 
         public void Publish(string queue, byte[] message)
         {
-            using (var connection = _factory.CreateConnection())
-            {
-                using (var channel = connection.CreateModel())
-                {
-                    //ensure queue is created
-                    channel.QueueDeclare(
-                        queue: queue,
-                        durable: false,
-                        exclusive: false,
-                        autoDelete: false,
-                        arguments: null
-                    );
+            //using (var connection = _factory.CreateConnection())
+            //{
+            //    using (var channel = connection.CreateModel())
+            //    {
+            //        //ensure queue is created
+            //        channel.QueueDeclare(
+            //            queue: queue,
+            //            durable: false,
+            //            exclusive: false,
+            //            autoDelete: false,
+            //            arguments: null
+            //        );
 
-                    //publish message
-                    channel.BasicPublish(
-                        exchange: "",
-                        routingKey: queue,
-                        basicProperties: null,
-                        body: message
-                    );
+            //        //publish message
+            //        channel.BasicPublish(
+            //            exchange: "",
+            //            routingKey: queue,
+            //            basicProperties: null,
+            //            body: message
+            //        );
 
                     
-                }
-            }
+            //    }
+            //}
         }
     }
 }
