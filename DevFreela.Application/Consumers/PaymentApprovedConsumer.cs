4b26f3353be3ee6111d106975451021a9680d5cc
Gleison <126490844+gvieira-dutra@users.noreply.github.com>
2024-09-10 13:37:57 -0400

    removing rabbitMQ references

--- DevFreela.Application/Consumers/PaymentApprovedConsumer.cs
+++ DevFreela.Application/Consumers/PaymentApprovedConsumer.cs
@@ -12,64 +12,64 @@ namespace DevFreela.Application.Consumers
 {
     public class PaymentApprovedConsumer : BackgroundService
     {
-        private const string PAYMENT_APPROVED_QUEUE = "PaymentsApproved";
-        private readonly IConnection _connection;
-        private readonly IModel _channel;
-        private readonly IServiceProvider _serviceProvider;
+        //private const string PAYMENT_APPROVED_QUEUE = "PaymentsApproved";
+        //private readonly IConnection _connection;
+        //private readonly IModel _channel;
+        //private readonly IServiceProvider _serviceProvider;
 
         public PaymentApprovedConsumer(IServiceProvider serviceProvider)
         {
-            _serviceProvider = serviceProvider;
+            //_serviceProvider = serviceProvider;
 
-            var factory = new ConnectionFactory
-            {
-                HostName = "localhost"
-            };
+            //var factory = new ConnectionFactory
+            //{
+            //    HostName = "localhost"
+            //};
 
-            _connection = factory.CreateConnection();
-            _channel = _connection.CreateModel();
+            //_connection = factory.CreateConnection();
+            //_channel = _connection.CreateModel();
 
-            _channel.QueueDeclare(
-                queue: PAYMENT_APPROVED_QUEUE,
-                durable: false,
-                exclusive: false,
-                autoDelete: false,
-                arguments: null
-            );
+            //_channel.QueueDeclare(
+            //    queue: PAYMENT_APPROVED_QUEUE,
+            //    durable: false,
+            //    exclusive: false,
+            //    autoDelete: false,
+            //    arguments: null
+            //);
         }
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            var consumer = new EventingBasicConsumer(_channel);
+            //var consumer = new EventingBasicConsumer(_channel);
 
-            consumer.Received += async (sender, eventArgs) =>
-            {
-                var paymentApprovedBytes = eventArgs.Body.ToArray();
-                var paymentsApprovedJson = Encoding.UTF8.GetString(paymentApprovedBytes);
+            //consumer.Received += async (sender, eventArgs) =>
+            //{
+            //    var paymentApprovedBytes = eventArgs.Body.ToArray();
+            //    var paymentsApprovedJson = Encoding.UTF8.GetString(paymentApprovedBytes);
 
-                var paymentApprovedIntegrationEvent = JsonSerializer.Deserialize<PaymentApprovedIntegrationEvent>(paymentsApprovedJson);
+            //    var paymentApprovedIntegrationEvent = JsonSerializer.Deserialize<PaymentApprovedIntegrationEvent>(paymentsApprovedJson);
 
-                await FinishProject(paymentApprovedIntegrationEvent.IdProject);
+            //    await FinishProject(paymentApprovedIntegrationEvent.IdProject);
 
-                _channel.BasicAck(eventArgs.DeliveryTag, false);
-            };
+            //    _channel.BasicAck(eventArgs.DeliveryTag, false);
+            //};
 
-            _channel.BasicConsume(PAYMENT_APPROVED_QUEUE, false, consumer);
+            //_channel.BasicConsume(PAYMENT_APPROVED_QUEUE, false, consumer);
 
             return Task.CompletedTask;
         }
 
         public async Task FinishProject(int id)
         {
-            using (var scope = _serviceProvider.CreateScope())
-            {
-                var projectRepository = scope.ServiceProvider.GetRequiredService<IProjectRepository>();
+            //using (var scope = _serviceProvider.CreateScope())
+            //{
+            //    var projectRepository = scope.ServiceProvider.GetRequiredService<IProjectRepository>();
 
-                var project = await projectRepository.GetByIdAsync(id);
+            //    var project = await projectRepository.GetByIdAsync(id);
 
-                project.Finish();
+            //    project.Finish();
 
-                await projectRepository.SaveChangesAsync();
-            }
+            //    await projectRepository.SaveChangesAsync();
+            //}
 
         }
     }
